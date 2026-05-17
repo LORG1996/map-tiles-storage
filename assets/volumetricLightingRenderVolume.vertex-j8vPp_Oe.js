eba3742945eaf87b564805c8d6adaff92bd3914e
Aleksander <87224115+LORG1996@users.noreply.github.com>
2026-05-17 14:36:37 +0300

    s
    s

---
+++ assets/volumetricLightingRenderVolume.vertex-j8vPp_Oe.js
@@ -0,0 +1,5 @@
+import{Bc as e,Fa as t,Ia as n,Pl as r,Vc as i}from"./index-BWQv8qGT.js";var a,o,s;r((()=>{i(),n(),t(),a=`volumetricLightingRenderVolumeVertexShader`,o=`#include<sceneUboDeclaration>
+#include<meshUboDeclaration>
+attribute position : vec3f;varying vWorldPos: vec4f;@vertex
+fn main(input : VertexInputs)->FragmentInputs {let worldPos=mesh.world*vec4f(vertexInputs.position,1.0);vertexOutputs.vWorldPos=worldPos;vertexOutputs.position=scene.viewProjection*worldPos;}
+`,e.ShadersStoreWGSL[a]||(e.ShadersStoreWGSL[a]=o),s={name:a,shader:o}}))();export{s as volumetricLightingRenderVolumeVertexShaderWGSL};
